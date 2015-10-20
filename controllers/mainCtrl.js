6d34f17e266e6b4ca93af9ab933f4b28c695c6bf
kelvinmbwilo <kelvinmbwilo@gmial.com>
2015-10-20 08:36:56 +0300

    initial commit

---
+++ controllers/mainCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kelvin on 10/20/15.
+ */
+angular.module("hmisPortal")
+    .config(function() {
+
+    })
+    .controller("mainCtrl",function ($scope,$http,$location,$timeout,DTOptionsBuilder) {
+
+    });
